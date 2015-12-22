e705da06015104b69a3adbc7e66ea907a88ea810
Camille Gardet <camille.gardet@c-s.fr>
2015-12-22 11:07:15 +0100

    Update to last chosen version

--- jquery-chosen-sortable.js
+++ jquery-chosen-sortable.js
@@ -11,54 +11,64 @@
  */
 (function($) {
 
-  $.fn.chosenOrder = function() {
-    var $this   = this.filter('.chzn-sortable[multiple]').first(),
-        $chosen = $this.siblings('.chzn-container');
+    $.fn.chosenOrder = function() {
+        var $this   = this.filter('.chosen-sortable[multiple]').first(),
+            $chosen = $this.siblings('.chosen-container'),
+            unselected = [],
+            sorted;
 
-    return $($chosen.find('.chzn-choices li[class!="search-field"]').map( function() {
-      if (!this) {
-        return undefined;
-      }
-      return $this.find('option:contains(' + $(this).text() + ')')[0];
-    }));
-  };
+        $this.find('option').each(function() {
+            !this.selected && unselected.push(this);
+        });
+
+        sorted = $($chosen.find('.chosen-choices li[class!="search-field"]').map(function() {
+            if (!this) {
+                return undefined;
+            }
+            var text = $.trim($(this).text());
+            return $this.find('option').filter(function() { return $(this).html() == text; })[0];
+        }));
+
+        sorted.push.apply(sorted, unselected);
+        return sorted;
+    };
 
 
-  /*
-   * Extend jQuery
-   */
-  $.fn.chosenSortable = function(){
-    var $this = this.filter('.chzn-sortable[multiple]');
+    /*
+     * Extend jQuery
+     */
+    $.fn.chosenSortable = function(){
+        var $this = this.filter('.chosen-sortable[multiple]');
 
-    $this.each(function(){
-      var $select = $(this);
-      var $chosen = $select.siblings('.chzn-container');
+        $this.each(function(){
+            var $select = $(this);
+            var $chosen = $select.siblings('.chosen-container');
 
-      // On mousedown of choice element,
-      // we don't want to display the dropdown list
-      $chosen.find('.chzn-choices').bind('mousedown', function(event){
-        if ($(event.target).is('span')) {
-          event.stopPropagation();
-        }
-      });
+            // On mousedown of choice element,
+            // we don't want to display the dropdown list
+            $chosen.find('.chosen-choices').bind('mousedown', function(event){
+                if ($(event.target).is('span')) {
+                    event.stopPropagation();
+                }
+            });
 
-      // Initialize jQuery UI Sortable
-      $chosen.find('.chzn-choices').sortable({
-        'placeholder' : 'ui-state-highlight',
-        'items'       : 'li:not(.search-field)',
-        //'update'      : _update,
-        'tolerance'   : 'pointer'
-      });
+            // Initialize jQuery UI Sortable
+            $chosen.find('.chosen-choices').sortable({
+                'placeholder' : 'ui-state-highlight',
+                'items'       : 'li:not(.search-field)',
+                //'update'      : _update,
+                'tolerance'   : 'pointer'
+            });
 
-      // Intercept form submit & order the chosens
-      $select.closest('form').on('submit', function(){
-        var $options = $select.chosenOrder();
-        $select.children().remove();
-        $select.append($options);
-      });
+            // Intercept form submit & order the chosens
+            $select.closest('form').on('submit', function(){
+                var $options = $select.chosenOrder();
+                $select.children().remove();
+                $select.append($options);
+                $select.trigger("chosen:updated");
+            });
 
-    });
+        });
 
-  };
-
+    };
 }(jQuery));
